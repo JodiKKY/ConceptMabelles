e5750f88f3b6daa90191645de55d78bd357ccd6c
JodiKKY <111978028+JodiKKY@users.noreply.github.com>
2025-06-07 03:28:13 +0000

    EE

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,15 +5,14 @@ import {
   signInWithEmailAndPassword, 
   signInWithPopup 
 } from "firebase/auth";
-import { getDatabase } from "firebase/database"; // ✅ Realtime DB
-import { getStorage } from "firebase/storage";   // ✅ Storage
-
+import { getDatabase } from "firebase/database"; 
+import { getStorage } from "firebase/storage";   
 const firebaseConfig = {
   apiKey: "AIzaSyC1PmiAfJRb9JNI4zub6cyNHk6aXFSQDFs",
   authDomain: "conceptmabelles.firebaseapp.com",
   databaseURL: "https://conceptmabelles-default-rtdb.firebaseio.com",
   projectId: "conceptmabelles",
-  storageBucket: "conceptmabelles.appspot.com", // ✅ fix ".app" to ".appspot.com"
+  storageBucket: "conceptmabelles.appspot.com", 
   messagingSenderId: "708848337518",
   appId: "1:708848337518:web:fb1ab4b8c429d367a51c7f"
 };
